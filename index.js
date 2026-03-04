7878f2077129ba609fceebe54708ea23cb827fa0
Geziel Cruz PC Safras <moraisgeziel785@gmail.com>
2026-03-04 17:42:55 -0300

    iPrimeira versão commitando Please enter the commit message for your changes. Lines starting

---
+++ index.js
@@ -0,0 +1,27 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    let filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url);
+
+    const extname = path.extname(filePath);
+
+    let contentType = 'text/html';
+    if (extname === '.css') contentType = 'text/css';
+    if (extname === '.js') contentType = 'text/javascript';
+
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            res.writeHead(404);
+            res.end('Arquivo não encontrado');
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content);
+        }
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Servidor rodando em http://localhost:3000');
+});
